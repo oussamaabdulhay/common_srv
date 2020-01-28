56dcba6926f43a94d86b32f9abb36750431321ed
CaptCrunch333 <addsd@live.com>
2020-01-28 11:07:52 +0400

    added the updated vector3d and vector2d

--- include/Vector3D.hpp
+++ include/Vector3D.hpp
@@ -1,9 +1,15 @@
+// Version: 3.0
+// Author: Mohamad Chehadeh
+// Last Modification By: Mohamad Wahbah
+// Date: 27 Jan 2020
+//Revision Note: Added assignment operator and constructor with init_lists, added  the "==" and "!=" logic operators
 #pragma once 
-
+template  <class L> class Vector2D;
 #include <cstdint>
+#include <initializer_list>
+#include "Vector2D.hpp"
 
 template <class T>  
-
 class Vector3D
 {
         public:
@@ -11,6 +17,8 @@ class Vector3D
                 T x = 0;
                 T y = 0;
                 T z = 0;
+                Vector3D<T>(){};
+                Vector3D<T>(std::initializer_list<T> tmp){this->x = *tmp.begin(); this->y = *(tmp.begin()+1); this->z = *(tmp.begin()+2);};
 
                 Vector3D<T> operator + (Vector3D<T> obj)
                 {
@@ -59,6 +67,24 @@ class Vector3D
                         this->z = tmp;
                 }
 
+                void operator = (std::initializer_list<T> tmp)
+                {
+                        this->x = *tmp.begin();
+                        this->y = *(tmp.begin()+1);
+                        this->z = *(tmp.begin()+2);
+                }
+
+                bool operator == (Vector3D<T> tmp)
+                {
+                        if(this->x == tmp.x && this->y == tmp.y && this->z == tmp.z) {return true;}
+                        else {return false;}
+                }
+
+                bool operator != (Vector3D<T> tmp){
+                        if(this->x != tmp.x || this->y != tmp.y || this->z != tmp.z) {return true;}
+                        else {return false;}
+                }
+
                 template <typename M>
                 operator Vector3D<M>()
                 {
@@ -70,4 +96,10 @@ class Vector3D
                         return tmp;
                 }
 
+                Vector2D<T> project_xy(){
+                        Vector2D<T> res;
+                        res.x=this->x;
+                        res.y=this->y;
+                        return res;
+                }
 };
